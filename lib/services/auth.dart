06c454fc9c0db962f0d31f12728416ec8883d488
Hope Kagame <kagame458@protonmail.com>
2023-03-12 16:03:15 +0300

    Update auth

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,18 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:hikersafrique/models/userclient.dart';
+import 'package:hikersafrique/models/client.dart';
+import 'package:hikersafrique/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   //to get client from firebase user
 
-  Client? _userFromFirebaseUser(User? user) {
-    return user != null ? Client(uid: user.uid) : null;
+  Client? _userFromFirebaseUser(User? user, bool signUp) {
+    return user != null
+        ? Client(
+            uid: user.uid,
+            clientName: user.displayName ?? 'User Name',
+            clientEmail: user.email!,
+            verified: signUp ? false : true,
+          )
+        : null;
   }
 
 // User stream to listen to auth changes
   Stream<Client?> get user {
-    return _auth.authStateChanges().map(_userFromFirebaseUser);
+    return _auth
+        .authStateChanges()
+        .map((user) => _userFromFirebaseUser(user, false));
   }
 
 //register with email & password
@@ -22,7 +32,9 @@ class AuthService {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       User user = result.user!;
-      return _userFromFirebaseUser(user);
+      // Saved registered user data
+      Database.saveClientData(_userFromFirebaseUser(user, true)!);
+      return _userFromFirebaseUser(user, true);
     } catch (e) {
       return null;
     }
@@ -35,7 +47,7 @@ class AuthService {
       UserCredential result = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
       User user = result.user!;
-      return _userFromFirebaseUser(user);
+      return _userFromFirebaseUser(user, false);
     } catch (e) {
       return null;
     }
