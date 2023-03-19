ea6a1541a4b3b8e90226a7c971b6c27cacfde4d0
Hope Kagame <kagame458@protonmail.com>
2023-03-19 17:44:15 +0300

    Show saved event

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hikersafrique/models/event.dart';
+import 'package:hikersafrique/services/auth_notifier.dart';
 import 'package:hikersafrique/services/database.dart';
 import 'package:hikersafrique/items/best_places.dart';
 import 'package:hikersafrique/items/favourites.dart';
@@ -10,6 +11,7 @@ import 'package:hikersafrique/items/restaurants.dart';
 import 'package:hikersafrique/screens/post_screen.dart';
 import 'package:hikersafrique/widgets/home_appbar.dart';
 import 'package:hikersafrique/widgets/home_bottombar.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -31,69 +33,24 @@ class HomeScreen extends StatelessWidget {
                   Expanded(
                     child: SizedBox(
                       height: 200.0,
-                      child: ListView.builder(
-                        itemCount: 10,
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemBuilder: (context, int index) {
-                          return GestureDetector(
-                            onTap: () {
-                              // Navigator.push(
-                              //     context,
-                              //     MaterialPageRoute(
-                              //       builder: (context) => const PostScreen(),
-                              //     ));
-                            },
-                            child: Container(
-                              height: 160.0,
-                              padding: const EdgeInsets.all(20.0),
-                              margin: const EdgeInsets.only(left: 15.0),
-                              decoration: BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(15.0),
-                                image: DecorationImage(
-                                    image: AssetImage(
-                                      'images/city${index + 1}.jpg',
-                                    ),
-                                    fit: BoxFit.cover,
-                                    opacity: 0.7),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  // bookmark icon
-                                  Container(
-                                    alignment: Alignment.topRight,
-                                    child: const Padding(
-                                      padding: EdgeInsets.only(top: 1.0),
-                                      child: Icon(
-                                          Icons.bookmark_border_outlined,
-                                          color: Colors.white,
-                                          size: 25.0),
-                                    ),
-                                  ),
-                                  const Spacer(),
-                                  // City Name
-                                  Container(
-                                    alignment: Alignment.bottomLeft,
-                                    child: const Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          vertical: 20.0),
-                                      child: Text(
-                                        'Hike',
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w700,
-                                            fontSize: 18.0),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
+                      child: FutureBuilder<List<Event>>(
+                          future: Database.getSavedEvents(
+                              Provider.of<AuthNotifier>(context)
+                                  .user!
+                                  .clientEmail),
+                          initialData: const [],
+                          builder: (context, snapshot) {
+                            return ListView.builder(
+                              itemCount: snapshot.data!.length,
+                              scrollDirection: Axis.horizontal,
+                              shrinkWrap: true,
+                              itemBuilder: (context, int index) {
+                                return SavedEvent(
+                                  event: snapshot.data![index],
+                                );
+                              },
+                            );
+                          }),
                     ),
                   ),
                 ],
@@ -119,7 +76,7 @@ class HomeScreen extends StatelessWidget {
                       // Hotels Container
                       HotelsContainer(),
                       // Restaurants Container
-                      RestaurantContainer()
+                      RestaurantContainer(),
                     ],
                   ),
                 ),
@@ -144,6 +101,84 @@ class HomeScreen extends StatelessWidget {
                   }),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class SavedEvent extends StatelessWidget {
+  const SavedEvent({
+    super.key,
+    required this.event,
+  });
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => PostScreen(
+                      event: event,
+                    )));
+      },
+      child: Container(
+        height: 160.0,
+        margin: const EdgeInsets.only(left: 15.0),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Stack(
+          children: [
+            // bookmark icon
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.network(
+                event.eventImageUrl,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                color: const Color(0xFF000000).withAlpha(100),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.topRight,
+              child: const Padding(
+                padding: EdgeInsets.only(top: 12, left: 12),
+                child: Icon(Icons.bookmark_border_outlined,
+                    color: Colors.white, size: 25.0),
+              ),
+            ),
+            const Spacer(),
+            // City Name
+            Container(
+              alignment: Alignment.bottomLeft,
+              padding: const EdgeInsets.only(left: 12),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Text(
+                  event.eventName,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
