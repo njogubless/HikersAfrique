4fcdbc66dcba6b80ec9ccf8273a14db5d8fc9c8f
Hope Kagame <kagame458@protonmail.com>
2023-03-19 16:55:17 +0300

    Remove categories

--- lib/screens/admin/admin_home.dart
+++ lib/screens/admin/admin_home.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hikersafrique/models/event.dart';
 import 'package:hikersafrique/services/database.dart';
-import 'package:hikersafrique/items/best_places.dart';
-import 'package:hikersafrique/items/favourites.dart';
-import 'package:hikersafrique/items/hotels.dart';
-import 'package:hikersafrique/items/most_visited.dart';
-import 'package:hikersafrique/items/new_added.dart';
-import 'package:hikersafrique/items/restaurants.dart';
 import 'package:hikersafrique/screens/post_screen.dart';
 import 'package:hikersafrique/widgets/home_appbar.dart';
 import 'package:hikersafrique/widgets/home_bottombar.dart';
@@ -28,29 +22,6 @@ class AdminHome extends StatelessWidget {
             children: [
               const SizedBox(
                 height: 20.0,
-              ),
-              // Category of best places,most visited etc
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    children: const [
-                      // BestPlaces Container
-                      BestPlacesContainer(),
-                      // Most Visited Container
-                      MostVisitedContainer(),
-                      //Favourites Container
-                      FavouriteContainer(),
-                      // New Added Container
-                      NewAddedContainer(),
-                      // Hotels Container
-                      HotelsContainer(),
-                      // Restaurants Container
-                      RestaurantContainer()
-                    ],
-                  ),
-                ),
               ),
               const SizedBox(
                 height: 10.0,
