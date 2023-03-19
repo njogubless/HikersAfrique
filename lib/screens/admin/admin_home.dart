7aa48749cb87263cbd474a094e63820aef29f1c8
Hope Kagame <kagame458@protonmail.com>
2023-03-19 16:52:54 +0300

    Add admin home page

---
+++ lib/screens/admin/admin_home.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:hikersafrique/models/event.dart';
+import 'package:hikersafrique/services/database.dart';
+import 'package:hikersafrique/items/best_places.dart';
+import 'package:hikersafrique/items/favourites.dart';
+import 'package:hikersafrique/items/hotels.dart';
+import 'package:hikersafrique/items/most_visited.dart';
+import 'package:hikersafrique/items/new_added.dart';
+import 'package:hikersafrique/items/restaurants.dart';
+import 'package:hikersafrique/screens/post_screen.dart';
+import 'package:hikersafrique/widgets/home_appbar.dart';
+import 'package:hikersafrique/widgets/home_bottombar.dart';
+
+class AdminHome extends StatelessWidget {
+  const AdminHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(90.0),
+        child: CustomHomeAppBar(),
+      ),
+      bottomNavigationBar: const CustomBottomNavBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20.0,
+              ),
+              // Category of best places,most visited etc
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: const [
+                      // BestPlaces Container
+                      BestPlacesContainer(),
+                      // Most Visited Container
+                      MostVisitedContainer(),
+                      //Favourites Container
+                      FavouriteContainer(),
+                      // New Added Container
+                      NewAddedContainer(),
+                      // Hotels Container
+                      HotelsContainer(),
+                      // Restaurants Container
+                      RestaurantContainer()
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              FutureBuilder<List<Event>>(
+                  future: Database.getAvailableEvents(),
+                  initialData: const [],
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemBuilder: (BuildContext context, index) {
+                        return EventItem(
+                          event: snapshot.data![index],
+                        );
+                      },
+                    );
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EventItem extends StatelessWidget {
+  const EventItem({
+    super.key,
+    required this.event,
+  });
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => PostScreen(
+                            event: event,
+                          )));
+            },
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.black26,
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: Image.network(
+                event.eventImageUrl,
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  event.eventName,
+                  style: const TextStyle(
+                      fontSize: 20.0, fontWeight: FontWeight.w500),
+                ),
+                const Icon(
+                  Icons.more_vert,
+                  size: 30.0,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 5.0,
+          ),
+          Row(
+            children: const [
+              Icon(
+                Icons.star,
+                color: Colors.amber,
+                size: 20.0,
+              ),
+              Text(
+                '4.5',
+                style: TextStyle(fontWeight: FontWeight.w600),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
