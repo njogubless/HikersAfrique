58ee96e5fa06d3d6de6d8d254f77fa5455f301c2
Hope Kagame <kagame458@protonmail.com>
2023-03-19 16:45:35 +0300

    Add pending approval page

--- lib/screens/home/client_admin_redirect.dart
+++ lib/screens/home/client_admin_redirect.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hikersafrique/models/client.dart';
 import 'package:hikersafrique/screens/home/home_screen.dart';
 import 'package:hikersafrique/services/auth_notifier.dart';
 import 'package:provider/provider.dart';
@@ -10,17 +11,71 @@ class ClientAdminRedirect extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = Provider.of<AuthNotifier>(context).user;
     try {
-      if (user!.role == 'client') {
-        return const HomeScreen();
+      if (!user!.verified) {
+        return PendingApprovalPage(user: user);
       } else {
-        return const Scaffold(
-          body: Center(child: Text('Admin')),
-        );
+        if (user.role == 'client') {
+          return const HomeScreen();
+        } else {
+          return const Scaffold(
+            body: Center(child: Text('Admin')),
+          );
+        }
       }
     } catch (e) {
       return const Scaffold(
         body: Center(),
       );
     }
+  }
+}
+
+class PendingApprovalPage extends StatelessWidget {
+  const PendingApprovalPage({
+    super.key,
+    required this.user,
+  });
+
+  final Client? user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Pending approval',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Hey, ${user!.clientName}',
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Your registration has not been approved yet! Please check back soon.',
+            overflow: TextOverflow.visible,
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
