1af8e6fb06064ff8199c9cc5306741549f73193a
Hope Kagame <kagame458@protonmail.com>
2023-03-22 13:27:18 +0300

    Add add_events page

---
+++ lib/screens/event_manager/add_event.dart
@@ -0,0 +1,170 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:hikersafrique/models/event.dart';
+import 'package:hikersafrique/services/database.dart';
+
+class AddEvents extends StatefulWidget {
+  const AddEvents({super.key});
+
+  @override
+  State<AddEvents> createState() => _AddEventsState();
+}
+
+class _AddEventsState extends State<AddEvents> {
+  final _formkey = GlobalKey<FormState>();
+  final _eventNameController = TextEditingController();
+  final _eventDateController = TextEditingController();
+  final _eventTimeController = TextEditingController();
+  final _eventCostController = TextEditingController();
+  final _eventLocationController = TextEditingController();
+  final _eventImageUrlController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          'Add Events',
+          style: TextStyle(
+            fontWeight: FontWeight.w900,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Form(
+                key: _formkey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: _eventNameController,
+                        decoration: const InputDecoration(
+                            labelText: 'EVENT NAME',
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.blueAccent))),
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                          controller: _eventDateController,
+                          decoration: const InputDecoration(
+                            labelText: 'EVENT DATE',
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Colors.blueAccent,
+                              ),
+                            ),
+                          )),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                        controller: _eventTimeController,
+                        decoration: const InputDecoration(
+                            labelText: 'EVENT TIME',
+                            labelStyle: TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.blueAccent))),
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                        controller: _eventCostController,
+                        decoration: const InputDecoration(
+                            labelText: 'EVENT COST',
+                            labelStyle: TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.blueAccent))),
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                        controller: _eventLocationController,
+                        decoration: const InputDecoration(
+                            labelText: 'EVENT LOCATION',
+                            labelStyle: TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.blueAccent))),
+                      ),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                        controller: _eventImageUrlController,
+                        decoration: const InputDecoration(
+                            labelText: 'EVENT IMAGE URL',
+                            labelStyle: TextStyle(
+                                fontWeight: FontWeight.bold, color: Colors.grey),
+                            focusedBorder: UnderlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.blueAccent))),
+                      ),
+                      const SizedBox(height: 45.0),
+                      InkWell(
+                        onTap: () async {
+                          await Database.createEvent(
+                            Event(
+                              eventCost: int.parse(_eventCostController.text),
+                              eventDate: _eventDateController.text,
+                              eventImageUrl: _eventImageUrlController.text,
+                              eventLocation: _eventLocationController.text,
+                              eventName: _eventNameController.text,
+                              eventTime: _eventTimeController.text,
+                            ),
+                          );
+                          _eventNameController.clear();
+                          _eventDateController.clear();
+                          _eventTimeController.clear();
+                          _eventCostController.clear();
+                          _eventLocationController.clear();
+                          _eventImageUrlController.clear();
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            content: Text('Event created!'),
+                            behavior: SnackBarBehavior.floating,
+                          ));
+                        },
+                        child: SizedBox(
+                          height: 40,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Colors.blue,
+                            elevation: 7.0,
+                            child: const Center(
+                              child: Text(
+                                'CREATE EVENT',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                    ],
+                  ),
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+}
