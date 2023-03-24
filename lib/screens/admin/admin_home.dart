a1e1b5a76fc204e285c8c991bdf88aaecb63a159
Hope Kagame <kagame458@protonmail.com>
2023-03-24 22:20:15 +0300

    Refactor admin home

--- lib/screens/admin/admin_home.dart
+++ lib/screens/admin/admin_home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hikersafrique/models/event.dart';
-import 'package:hikersafrique/services/auth.dart';
-import 'package:hikersafrique/services/database.dart';
+import 'package:hikersafrique/screens/admin/event_management.dart';
+import 'package:hikersafrique/screens/admin/finance_management.dart';
 import 'package:hikersafrique/widgets/admin_bottombar.dart';
 
 class AdminHome extends StatelessWidget {
@@ -9,266 +8,22 @@ class AdminHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final pageNotifier = ValueNotifier<int>(0);
     return Scaffold(
       appBar: const PreferredSize(
         preferredSize: Size.fromHeight(90.0),
         child: AdminAppBar(),
       ),
-      bottomNavigationBar: const AdminBottomNavBar(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15, right: 15),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20.0,
-              ),
-              const TotalRevenueSection(),
-              const SizedBox(height: 20.0),
-              Row(
-                children: const [
-                  Text(
-                    'Revenue from events',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20.0),
-              FutureBuilder<List<Event>>(
-                  future: Database.getAvailableEvents(),
-                  initialData: const [],
-                  builder: (context, snapshot) {
-                    return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, index) {
-                        return EventItem(
-                          event: snapshot.data![index],
-                        );
-                      },
-                    );
-                  }),
-            ],
-          ),
-        ),
+      bottomNavigationBar: AdminBottomNavBar(
+        pageNotifier: pageNotifier,
       ),
-    );
-  }
-}
-
-class EventItem extends StatelessWidget {
-  const EventItem({
-    super.key,
-    required this.event,
-  });
-
-  final Event event;
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<int>(
-        future: Database.getNumberBooked(event.eventID),
-        initialData: 0,
-        builder: (context, snapshot) {
-          final numberBooked = snapshot.data!;
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 15.0),
-            child: Container(
-              height: 130,
-              width: double.infinity,
-              padding: const EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            event.eventName,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.w800,
-                              fontSize: 20,
-                            ),
-                          ),
-                          Text(
-                            'Unit amount(Ksh): ${event.eventCost}',
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                          Text(
-                            'Clients booked: $numberBooked',
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Ksh. ${event.eventCost * numberBooked}',
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 20),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        });
-  }
-}
-
-class TotalRevenueSection extends StatelessWidget {
-  const TotalRevenueSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 15.0),
-      child: Container(
-        height: 130,
-        width: double.infinity,
-        padding: const EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: Colors.blueGrey,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Total Revenue',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 20,
-                      ),
-                    ),
-                    FutureBuilder<int>(
-                        future: Database.getNumberOfEvents(),
-                        initialData: 0,
-                        builder: (context, snapshot) {
-                          final no = snapshot.data!;
-                          return Text(
-                            'No. of events: $no',
-                            style: const TextStyle(color: Colors.white),
-                          );
-                        }),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FutureBuilder<int>(
-                    future: Database.getTotalRevenue(),
-                    initialData: 0,
-                    builder: (context, snapshot) {
-                      final total = snapshot.data!;
-                      return Text(
-                        'Ksh. $total',
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 20),
-                      );
-                    }),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class AdminAppBar extends StatelessWidget {
-  const AdminAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final AuthService auth = AuthService();
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Sort
-            InkWell(
-              onTap: () {},
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 6.0,
-                      )
-                    ],
-                    borderRadius: BorderRadius.circular(15.0)),
-                child: const Icon(Icons.sort_rounded, size: 28.0),
-              ),
-            ),
-            // Location and City
-            Row(
-              children: const [
-                // Location on icon
-                Icon(
-                  Icons.supervised_user_circle_rounded,
-                  color: Color(0xFFF65959),
-                ),
-                // City Text
-                Text(
-                  'Admin',
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),
-                )
-              ],
-            ),
-            // Search icon
-            DropdownButton<int>(
-              icon: const Icon(Icons.more_vert),
-              underline: const SizedBox.shrink(),
-              items: const [
-                DropdownMenuItem(
-                  value: 1,
-                  child: Text(
-                    'Logout',
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 16,
-                      fontFamily: 'AvenirNext',
-                    ),
-                  ),
-                ),
-              ],
-              onChanged: (selection) {
-                auth.signOut();
-              },
-            ),
-          ],
-        ),
-      ),
+      body: ValueListenableBuilder<int>(
+          valueListenable: pageNotifier,
+          builder: (context, page, _) {
+            return page == 0
+                ? const FinanceManagement()
+                : const EventManagement();
+          }),
     );
   }
 }
