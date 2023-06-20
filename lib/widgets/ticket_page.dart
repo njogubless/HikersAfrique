d00c53e5e50cf1845fae7c7dc4f7e9b732e2a51b
Hope Kagame <hopekagame1@gmail.com>
2023-06-20 06:31:59 +0300

    Add ticket page

---
+++ lib/widgets/ticket_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:hikersafrique/constant.dart';
+import 'package:hikersafrique/models/event.dart';
+import 'package:hikersafrique/services/auth_notifier.dart';
+import 'package:hikersafrique/widgets/payment_page.dart';
+import 'package:provider/provider.dart';
+
+class TicketPage extends StatelessWidget {
+  const TicketPage({super.key, required this.event});
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<AuthNotifier>(context).user;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.check,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            const Text(
+              'Successfully booked!',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Please check back soon',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(height: 20),
+            SecondaryButton(
+              isPrimary: true,
+              title: 'Download ticket',
+              onPressed: () {
+                Misc.getReceipt(event, user!).then((_) =>
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      backgroundColor: Colors.greenAccent,
+                      content: Text('Find your receipt in your Downloads!'),
+                    )));
+              },
+            ),
+            const SizedBox(height: 20),
+            SecondaryButton(
+              title: 'Finish',
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
