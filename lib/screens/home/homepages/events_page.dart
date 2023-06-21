947b733cd9acb0b675b66c88c921f77b633eaa7e
dev-vickie <vmutethia84@gmail.com>
2023-06-21 18:25:22 +0300

    Add fonts/drawer and bottom nav

--- lib/screens/home/homepages/events_page.dart
+++ lib/screens/home/homepages/events_page.dart
@@ -27,34 +27,6 @@ class _EventsPageState extends State<EventsPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                      height: 200.0,
-                      child: FutureBuilder<List<Event>>(
-                          future: Database.getSavedEvents(
-                              Provider.of<AuthNotifier>(context)
-                                  .user!
-                                  .clientEmail),
-                          initialData: const [],
-                          builder: (context, snapshot) {
-                            return ListView.builder(
-                              padding: const EdgeInsets.only(right: 20),
-                              itemCount: snapshot.data!.length,
-                              scrollDirection: Axis.horizontal,
-                              shrinkWrap: true,
-                              itemBuilder: (context, int index) {
-                                return SavedEvent(
-                                  event: snapshot.data![index],
-                                );
-                              },
-                            );
-                          }),
-                    ),
-                  ),
-                ],
-              ),
               const SizedBox(
                 height: 20.0,
               ),
--- lib/screens/home/homepages/favorites.dart
+++ lib/screens/home/homepages/favorites.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hikersafrique/screens/home/home_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/event.dart';
+import '../../../services/auth_notifier.dart';
+import '../../../services/database.dart';
 
 class Favorites extends StatefulWidget {
   const Favorites({super.key});
@@ -11,7 +17,34 @@ class _FavoritesState extends State<Favorites> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Favorites page")),
+      body: Row(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: FutureBuilder<List<Event>>(
+                  future: Database.getSavedEvents(
+                      Provider.of<AuthNotifier>(context).user!.clientEmail),
+                  initialData: const [],
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      padding: const EdgeInsets.only(right: 20),
+                      itemCount: snapshot.data!.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          child: SavedEvent(
+                            event: snapshot.data![index],
+                          ),
+                        );
+                      },
+                    );
+                  }),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
