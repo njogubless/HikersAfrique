b6a52c0fc0c2716922975c70dc9ee96e03903f9e
njogubless <pnjogubless@gmail.com>
2023-09-04 20:51:04 +0300

    updating the lipanampesa code

--- lib/components/lipanampesa.dart
+++ lib/components/lipanampesa.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: override_on_non_overriding_member, library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
 import 'package:mpesa_flutter_plugin/mpesa_flutter_plugin.dart';
 
-class LipanaMpesa extends StatefulWidget {
-  const LipanaMpesa(
+class LipaNaMpesa extends StatefulWidget {
+  const LipaNaMpesa(
       {required this.consumerKey, required this.consumerSecret, Key? key})
       : super(key: key);
 
@@ -12,21 +10,53 @@ class LipanaMpesa extends StatefulWidget {
   final String consumerSecret;
 
   @override
-  _LipanaMpesaState createState() => _LipanaMpesaState();
+  _LipaNaMpesaState createState() => _LipaNaMpesaState();
 }
 
-class _LipanaMpesaState extends State<LipanaMpesa> {
+class _LipaNaMpesaState extends State<LipaNaMpesa> {
   @override
-  void initstate() {
+  void initState() {
     super.initState();
     MpesaFlutterPlugin.setConsumerKey(widget.consumerKey);
     MpesaFlutterPlugin.setConsumerSecret(widget.consumerSecret);
   }
 
+  Future<void> _lipaNaMpesa() async {
+    try {
+      final transactionInitialization =
+          await MpesaFlutterPlugin.initializeMpesaSTKPush(
+        businessShortCode: "174379",
+        transactionType: TransactionType.CustomerPayBillOnline,
+        amount: 1.0,
+        partyA: "254792964487",
+        partyB: "174379",
+        callBackURL: Uri(
+            scheme: "https",
+            host: "mpesa-requestbin.herokuapp.com",
+            path: "/1hhy6391"),
+        accountReference: " Hikers Afrique",
+        phoneNumber: "0746179799",
+        baseUri: Uri(scheme: "https", host: "sandbox.safaricom.co.ke"),
+        transactionDesc: "purchase",
+        passKey:
+            "A1d5SfyYCLMFnRvULne7ii5DeqGuYKQ8NnBty7TKkUlEGSgdx6u96lxX3ujo/+lzG6pD2ShvISQe578pZ4hTH68KAUabfngZUituRzKEqh1JP3J4d/mX1Zl+D08Fd/GArwzpHSrRW30LxTXvfrQ+H56gm5mcyv1aBhWBggh8P6YTDbrlfg58L9ht0+c8+7mFRubNQWVWRKhMW5HijMI69G+dX2QJeLNJk8KEyZAuvJFUtuebsMtsvO1Xdf9347PzDQCxR/ry70S5M8LeVx6XaPvtl+4waI69f8EwWD90s3zd7GxyBBbG4LE5IhuRpC7MsTUIMymrZBaga/4pMu7gDA==",
+      );
+
+      if (transactionInitialization != null) {
+        print('Mpesa transaction initialized successfully!');
+      } else {
+        print('Mpesa transaction initialization failed.');
+      }
+    } catch (e) {
+      print("CAUGHT EXCEPTION: $e");
+      rethrow;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<void>(
-      future: lipaNaMpesa(),
+      future: _lipaNaMpesa(),
       builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const CircularProgressIndicator();
@@ -37,36 +67,5 @@ class _LipanaMpesaState extends State<LipanaMpesa> {
         }
       },
     );
-  }
-
-  Future<void> lipaNaMpesa() async {
-    dynamic transactionInitialization;
-    try {
-      dynamic widget;
-      MpesaFlutterPlugin.setConsumerKey(widget.consumerKey);
-      MpesaFlutterPlugin.setConsumerSecret(widget.consumerSecret);
-
-      transactionInitialization =
-          await MpesaFlutterPlugin.initializeMpesaSTKPush(
-        businessShortCode: "174379",
-        transactionType: TransactionType.CustomerPayBillOnline,
-        amount: 1.0,
-        partyA: "0746179799",
-        partyB: "174379",
-        callBackURL:
-            Uri.parse("https://mpesa-requestbin.herokuapp.com/1hhy6391"),
-        accountReference: "Hikers Afrique",
-        phoneNumber: "0746179799",
-        baseUri: Uri.parse("https://sandbox.safaricom.co.ke"),
-        transactionDesc: "purchase",
-        passKey:
-            "XKRmoyvapv+XPvfuJafq6thvHG1JuJBnn9TTBKugrIWpDXGlfyHNGo3vPmH1iM2LL57SA7hsjqI0dK7Wl3tAVhNduaGlKfyHK7tNGF46CwRC4LcwfqngYcYi+Rrx/fVZYNOHNWTWEmw4CjwoeeEB40z+zUEfElHfFyWeUBC1GDI3cwoH9+pLkJHhowlicBr3WTlvtx8wdWRUZ1efbYjsDMiGu76fwlIQHzXNy+nqPJPuMFfw6/rEHPwGwwXQ+rFlbkrRHUKA0teaJUaqE6nCNfELIEPPYFXwo+dAVM7LFhT1hfRr8emcm57hGjQMA3Y0F4JmPNl5AFrM5ThMJi5nDA==",
-      );
-
-      return transactionInitialization;
-    } catch (e) {
-      print("CAUGHT EXCEPTION: $e");
-      rethrow;
-    }
   }
 }
